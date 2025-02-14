e366d6f22e8d088bc3f3a6a92ae72a658ecea65d
Daniel Perez Gonzalez <isc.pgd@gmail.com>
2025-02-13 18:14:41 -0600

    Feat: uso de API use de React Pt1

--- src/pages/Planets.tsx
+++ src/pages/Planets.tsx
@@ -1,53 +1,38 @@
-import { FC, useEffect, useState } from 'react';
-import { planetsApi } from '../api/planetsApi';
+import { FC, use } from 'react';
 import { Planet } from '../interfaces/planet.interface';
 import { EditPlanetForm } from './ui/EditPlanetForm';
 import { PlanetList } from './ui/PlanetList';
 
-const getPlanets = async () => {
-  const res = await planetsApi.get('/');
-  return res.data;
-};
+interface Props {
+    getPlanets: Promise<Planet[]>
+}
 
-const Planets: FC = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const [planets, setPlanets] = useState<Planet[]>([]);
+const Planets: FC<Props> = ({ getPlanets }) => {
+    const planets: Planet[] = use(getPlanets);
 
-  useEffect(() => {
-    getPlanets()
-      .then((res) => {
-        setPlanets(res);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setIsLoading(false);
-      });
-  }, []);
+    const handleAddPlanet = (planet: Partial<Planet>) => {
+        console.log(planet);
+    };
 
-  const handleAddPlanet = (planet: Partial<Planet>) => {
-    console.log(planet);
-  };
+    return (
+        <>
+            <h4 className="text-2xl font-thin mb-4">Agregar y mantener planetas</h4>
+            <hr className="border-gray-300 mb-4" />
+            {/* Formulario para agregar un planeta */}
+            <EditPlanetForm onAddPlanet={handleAddPlanet} />
 
-  return (
-    <>
-      <h4 className="text-2xl font-thin mb-4">Agregar y mantener planetas</h4>
-      <hr className="border-gray-300 mb-4" />
-      {/* Formulario para agregar un planeta */}
-      <EditPlanetForm onAddPlanet={handleAddPlanet} />
+            {/* {error && (
+                <p>
+                    Error al cargar los planetas -{' '}
+                    <small className="text-red-500">{error}</small>
+                </p>
+            )} */}
 
-      {error && (
-        <p>
-          Error al cargar los planetas -{' '}
-          <small className="text-red-500">{error}</small>
-        </p>
-      )}
-
-      {/* Lista de planetas Grid*/}
-      {isLoading ? <p>Cargando...</p> : <PlanetList planets={planets} />}
-    </>
-  );
+            {/* Lista de planetas Grid*/}
+            {/* {isLoading ? <p>Cargando...</p> : <PlanetList planets={planets} />} */}
+            <PlanetList planets={planets} />
+        </>
+    );
 };
 
 export default Planets;
